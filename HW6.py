e2a2485207ec5e23235bb425bf3d6cdd287214c8
Huley Harmon <hh128060@student.wpsok.org>
2026-09-16 13:52:05 -0500

    Turned in HW6

---
+++ HW6.py
@@ -0,0 +1,27 @@
+#Name:Huxley Harmon
+#Class: 5th Hour
+#Assignment: HW6
+
+
+#1. Create a list with 9 different numbers inside.
+Cat=[2,1,4,6,8,5,9,7]
+#2. Sort the list from highest to lowest.
+Cat.sort(reverse=True)
+#3. Create an empty list.
+Dog=[]
+#4. Remove the median number from the first list and add it to the second list.
+Plane=Cat.pop(4)
+Dog.append(Plane)
+#5. Remove the first number from the first list and add it to the second list.
+Lion=Dog.pop(4)
+Cat.append(Lion)
+#6. Print both lists.
+print(Dog, Dog)
+#7. Add the two numbers in the second list together and print the result.
+Tiger=Dog[5]=Cat[2]
+print(Tiger)
+#8. Add the sum from #7 to the first list.
+Dog.append(Tiger)
+#9. Sort the first list from lowest to highest and print it.
+Cat.sort()
+print(Cat)
